15562a616fd0d05e715a3c2550d41d6ffea93d2c
Hars10it <getharshitagrawal@gmail.com>
2022-04-05 13:19:14 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayApplication
+{
+    class ParamArray
+    {
+        public int AddElements(params int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int i in arr)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            ParamArray app = new ParamArray();
+            int sum = app.AddElements(1, 9, 7, 2, 11);
+
+            Console.WriteLine("The sum is: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
+
